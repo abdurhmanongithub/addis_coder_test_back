22f5420dd07751e4ec45258e4adb11822f007f05
Abdurhman <findabdurhman@gmail.com>
2024-08-26 20:12:00 +0300

    finalizing

--- src/routes/songRoutes.ts
+++ src/routes/songRoutes.ts
@@ -3,19 +3,14 @@ import { getSongs, getSongById, createSong, updateSong, deleteSong } from '../co
 
 const router = Router();
 
-// Route to get all songs
 router.get('/', getSongs);
 
-// Route to get a specific song by ID
 router.get('/:id', getSongById);
 
-// Route to create a new song
 router.post('/', createSong);
 
-// Route to update an existing song by ID
 router.put('/:id', updateSong);
 
-// Route to delete a song by ID
 router.delete('/:id', deleteSong);
 
 export default router;
